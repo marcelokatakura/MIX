c6659bc89ca49ecf543ecba90a4050a7271ab64d
marcelokatakura <marcelomkatakura@gmail.com>
2024-10-09 20:09:47 -0300

    Fix bug carrousel

--- source/script/carouselscript.js
+++ source/script/carouselscript.js
@@ -3,7 +3,7 @@ let currentSlide = 0;
 function moveSlide(step) {
     const slides = document.querySelectorAll('.carousel-slide #img1');
     const totalSlides = slides.length;
-    const visibleSlides = 4; // Number of visible slides at a time
+    const visibleSlides = 7; // Number of visible slides at a time
     const slideWidth = slides[0].clientWidth;
 
     // Calculate the next slide index
@@ -15,6 +15,14 @@ function moveSlide(step) {
     } else if (currentSlide > totalSlides - visibleSlides) {
         currentSlide = 0; // Move to the beginning of the slides
     }
+    const pvbuton = document.querySelector('.prev-btn')
+    const nxbuton = document.querySelector('.next-btn')
+    if (visibleSlides < 8) {
+        nxbuton.style.display = "none";}
+
+    pvbuton.addEventListener('click', function(){
+        nxbuton.style.display = "inline";
+    })
 
     // Update slide container position
     document.querySelector('.carousel-slide').style.transform = `translateX(${-slideWidth * currentSlide}px)`;
@@ -22,12 +30,13 @@ function moveSlide(step) {
     // Show/Hide buttons based on current slide position
     document.querySelector('.prev-btn').style.display = currentSlide === 0 ? 'none' : 'block';
 
+
 }
 
 function moveSlide1(step) {
     const slides = document.querySelectorAll('.carousel-slide1 img');
     const totalSlides = slides.length;
-    const visibleSlides = 4; // Number of visible slides at a time
+    const visibleSlides = 7; // Number of visible slides at a time
     const slideWidth = slides[0].clientWidth;
 
     // Calculate the next slide index
@@ -47,6 +56,7 @@ function moveSlide1(step) {
     document.querySelector('.prev-btn1').style.display = currentSlide === 0 ? 'none' : 'block';
 
 }
+
 
 window.onload = () => {
     const slides = document.querySelectorAll('.carousel-slide #img1, .carousel-slide1 ');
